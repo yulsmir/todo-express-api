880f4fd1708062aef03303bcdbdd296dc3f2990e
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-23 15:57:06 +0200

    Remove console.error(err) from errorHandlers

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -1,12 +1,10 @@
 'use strict';
 
 const handleFileReadError = (err, res) => {
-  console.error(err);
   res.status(500).json({ error: 'Failed to read todos file.' });
 };
 
 const handleFileWriteError = (err, res) => {
-  console.error(err);
   res.status(500).json({ error: 'Failed to write todos file.' });
 };
 
